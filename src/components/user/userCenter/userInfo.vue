a40e92d84aa20aa175becfa94b8e8b35f0e3f7ea
snoopy1412 <402090261@qq.com>
2018-03-08 13:55:13 +0800

    fix: 验证邮箱默认关闭

--- src/components/user/userCenter/userInfo.vue
+++ src/components/user/userCenter/userInfo.vue
@@ -71,7 +71,7 @@
       return {
         breadcrumbText: this.$t("user.info"),
         user: {},
-        pop_email: true,
+        pop_email: false,
         pop_phone: false,
         pop_phone_show: false
       };
