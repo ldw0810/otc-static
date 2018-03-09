59c17b76ab03d05031d8eda6a4fcd29e65938e9f
snoopy1412 <402090261@qq.com>
2018-03-09 13:18:08 +0800

    fix: 资产页面初始化报错

--- src/components/asset/asset.vue
+++ src/components/asset/asset.vue
@@ -646,7 +646,7 @@ export default {
       Object.keys(this.form).map(item => {
         this.form[item] = ''
       })
-      this.$refs["form"].resetFields();
+      this.$refs["form"] && this.$refs["form"].resetFields();
     },
     handleAllWithdrawal() {
       this.form.number = this.amount;
@@ -797,16 +797,16 @@ export default {
     },
     checkAddress() {
       return new Promise((resolve, reject) => {
-        this.$refs["form"].validate(valid => {
-          if (valid) {
-            resolve();
-          } else {
-            this.$Message.error(
-              this.$t("asset.asset_withdraw_address_notValid")
-            );
-            reject();
-          }
-        });
+          this.$refs["form"].validate(valid => {
+            if (valid) {
+              resolve();
+            } else {
+              this.$Message.error(
+                this.$t("asset.asset_withdraw_address_notValid")
+              );
+              reject();
+            }
+          });
       });
     },
     setDefaultAddress(id) {
