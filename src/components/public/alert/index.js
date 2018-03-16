f416dc9ec647a3380d864bcf6c861863e80aee0f
liudewei <3151081884@qq.com>
2018-03-16 18:38:20 +0800

    1、alert框修改 2、二次验证超时时，跳回登陆页面

--- src/components/public/alert/index.js
+++ src/components/public/alert/index.js
@@ -1,27 +1,30 @@
 import Vue from 'vue'
 import alert from './alert'
 
-const alertController = Vue.extend(alert)
+const alertController = Vue.extend(alert);
 
 let alertInstance = function(options) {
-  const instance = new alertController().$mount(document.createElement('div'))
-  document.body.appendChild(instance.$el)
-  instance.visible = true
+  const instance = new alertController().$mount(document.createElement('div'));
+  document.body.appendChild(instance.$el);
+  instance.visible = true;
   if (options.title) {
-    instance.title = options.title
+    instance.title = options.title;
   }
   if (options.content) {
-    instance.content = options.content
+    instance.content = options.content;
   }
   if (options.okText) {
-    instance.okText = options.okText
+    instance.okText = options.okText;
   }
-  return instance
-}
+  if(options.callback) {
+    instance.callback = options.callback;
+  }
+  return instance;
+};
 
 alertInstance.error = function(options) {
-  const instance = alertInstance(options)
-  instance.type = 'error'
-}
+  const instance = alertInstance(options);
+  instance.type = 'error';
+};
 
-export default alertInstance
+export default alertInstance;
