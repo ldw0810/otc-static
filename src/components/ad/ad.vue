d5393bd02be14c018fecb160dd1d4cb6f49be9ea
liudewei <3151081884@qq.com>
2018-03-16 13:33:27 +0800

    修改广告时，不允许修改货币单位

--- src/components/ad/ad.vue
+++ src/components/ad/ad.vue
@@ -65,7 +65,7 @@
           <Row>
             <i-col span='10'>
               <Select class="input" v-model="form_buy.targetCurrency" :placeholder='$t("public.select")'
-                      @on-change="changeTargetCurrency" v-if="+adType === 0">
+                      @on-change="changeTargetCurrency" :disabled="isUpdate" v-if="+adType === 0">
                 <Option v-for="item in targetCurrencyList" :value="item" :key="item">
                   {{$t("public['" + item + "']")}}&nbsp;&nbsp;{{$t("public['" + item + "_text']")}}
                 </Option>
@@ -231,7 +231,7 @@
           <Row>
             <i-col span='10'>
               <Select class="input" v-model="form_sell.targetCurrency" @on-change="changeTargetCurrency"
-                      v-if="+adType === 1">
+                      :disabled="isUpdate" v-if="+adType === 1">
                 <Option v-for="item in targetCurrencyList" :value="item" :key="item">
                   {{$t("public['" + item + "']")}}&nbsp;&nbsp;{{$t("public['" + item + "_text']")}}
                 </Option>
@@ -815,16 +815,12 @@
                   id: this.adId,
                   max: this.form.ceiling,
                   min: this.form.floor,
-                  price:
-                    +this.adType === 0 ? this.form.maxPrice : this.form.minPrice,
+                  price: +this.adType === 0 ? this.form.maxPrice : this.form.minPrice,
                   margin: this.form.premium,
-                  pay_kind:
-                    +this.adType === 0 ? this.form.payment : this.form.collection,
+                  pay_kind: +this.adType === 0 ? this.form.payment : this.form.collection,
                   pay_default:
                     this.collection_default &&
-                    this.collection_default.id === this.form.collection
-                      ? 1
-                      : 0,
+                    this.collection_default.id === this.form.collection ? 1 : 0,
                   remark: this.form.remark
                 };
                 this.$store
