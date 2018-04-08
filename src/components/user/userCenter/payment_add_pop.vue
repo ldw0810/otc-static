98bc84952f053de36390a8d45f8b871ab035728a
liudewei <3151081884@qq.com>
2018-04-08 12:06:06 +0800

    我的广告页面，添加限额展示

--- src/components/user/userCenter/payment_add_pop.vue
+++ src/components/user/userCenter/payment_add_pop.vue
@@ -1,3 +1,4 @@
+0
 <template>
   <div id="popPage">
     <logoDiv></logoDiv>
@@ -121,374 +122,374 @@
   </div>
 </template>
 <script type="es6">
-import validateMixin from "@/components/mixins/validate-mixin";
-import {
-  VALI_ALIPAY_NAME,
-  VALI_ALIPAY_ACCOUNT,
-  VALI_CARD_HOLDER,
-  VALI_CARD_NUMBER
-} from "config/validator";
-import debounce from "lodash/debounce";
-import logoDiv from "../../public/logo.vue";
+  import validateMixin from "@/components/mixins/validate-mixin";
+  import {
+    VALI_ALIPAY_NAME,
+    VALI_ALIPAY_ACCOUNT,
+    VALI_CARD_HOLDER,
+    VALI_CARD_NUMBER
+  } from "config/validator";
+  import debounce from "lodash/debounce";
+  import logoDiv from "../../public/logo.vue";
 
-export default {
-  mixins: [validateMixin(["alipayForm", "bankCardForm"])],
-  props: {
-    popUpStatus: true
-  },
-  data() {
-    const validateReAccount = (rule, value, callback) => {
-      if (!value || !value.length) {
-        callback(new Error(this.$t("user.alipay_reAccount_required")));
-      } else if (value !== this.alipayForm.account) {
-        callback(new Error(this.$t("user.alipay_account_different")));
-      } else {
-        callback();
-      }
-    };
-    const validateReNumber = (rule, value, callback) => {
-      if (!value || !value.length) {
-        callback(new Error(this.$t("user.bankCard_reNumber_required")));
-      } else if (value !== this.bankCardForm.number) {
-        callback(new Error(this.$t("user.bankCard_number_different")));
-      } else {
-        if (value === this.bankCardForm.number && this.bankCardForm.number !== 0) {
-          this.$refs.bankCardForm.resetField('number')
+  export default {
+    mixins: [validateMixin(["alipayForm", "bankCardForm"])],
+    props: {
+      popUpStatus: true
+    },
+    data() {
+      const validateReAccount = (rule, value, callback) => {
+        if (!value || !value.length) {
+          callback(new Error(this.$t("user.alipay_reAccount_required")));
+        } else if (value !== this.alipayForm.account) {
+          callback(new Error(this.$t("user.alipay_account_different")));
+        } else {
+          callback();
         }
-        callback();
-      }
-    };
-    const validateReBank = (rule, value, callback) => {
-      if (!value || !value.length) {
-        callback(new Error(this.$t("user.bankCard_reNumber_required")));
-      } else if (value !== this.bankCardForm.reNumber && this.bankCardForm.reNumber !== '') {
-        callback(new Error(this.$t("user.bankCard_number_different")));
-      } else {
-        if (value === this.bankCardForm.reNumber && this.bankCardForm.reNumber !== 0) {
-          this.$refs.bankCardForm.resetField('reNumber')
+      };
+      const validateReNumber = (rule, value, callback) => {
+        if (!value || !value.length) {
+          callback(new Error(this.$t("user.bankCard_reNumber_required")));
+        } else if (value !== this.bankCardForm.number) {
+          callback(new Error(this.$t("user.bankCard_number_different")));
+        } else {
+          if (value === this.bankCardForm.number && this.bankCardForm.number !== 0) {
+            this.$refs.bankCardForm.resetField('number');
+          }
+          callback();
         }
-        callback();
-      }
-    };
+      };
+      const validateReBank = (rule, value, callback) => {
+        if (!value || !value.length) {
+          callback(new Error(this.$t("user.bankCard_reNumber_required")));
+        } else if (value !== this.bankCardForm.reNumber && this.bankCardForm.reNumber !== '') {
+          callback(new Error(this.$t("user.bankCard_number_different")));
+        } else {
+          if (value === this.bankCardForm.reNumber && +this.bankCardForm.reNumber !== 0) {
+            this.$refs.bankCardForm.resetField('reNumber');
+          }
+          callback();
+        }
+      };
 
-    const validateBank = (rule, value, callback) => {
-      let tvalue = Number(value).toString();
-      let verify = true;
-      let sum = 0;
-      if (tvalue.length < VALI_CARD_NUMBER.min || tvalue.length > VALI_CARD_NUMBER.max) {
-        verify = false;
-      }
-      // else {
-      //     for (let i = 0; i <= tvalue.length - 2; i++) {
-      //         if (i % 2 === 0) {
-      //             sum += (Number(tvalue[i]) * 2) % 10 + parseInt((Number(tvalue[i]) * 2) / 10)
-      //         } else {
-      //             sum += Number(tvalue[i])
-      //         }
-      //     }
-      //     verify = 10 - sum % 10 === Number(tvalue[tvalue.length - 1])
-      // }
-      if (!verify) {
-        callback(new Error(this.$t("user.bankCard_number_invalid")));
-      } else {
-        callback();
-      }
-    };
-    return {
-      contentIndex: 0,
-      alipayForm: {
-        userName: "",
-        account: "",
-        reAccount: ""
-      },
-      alipayFormLoading: false,
-      bankCardForm: {
-        userName: "",
-        bank: "",
-        number: "",
-        reNumber: ""
-      },
-      bankCardFormLoading: false,
-      alipayRules: {
-        userName: [
-          {
-            required: true,
-
-            message: this.$t("user.alipay_userName_required")
-          },
-          {
-            min: VALI_ALIPAY_NAME.min,
-            message: VALI_ALIPAY_NAME.message
-          },
-          {
-            max: VALI_ALIPAY_NAME.max,
-            message: VALI_ALIPAY_NAME.message
-          }
-        ],
-        account: [
-          {
-            required: true,
-            message: this.$t("user.alipay_account_required")
-          },
-          {
-            min: VALI_ALIPAY_ACCOUNT.min,
-            message: VALI_ALIPAY_ACCOUNT.message
-          },
-          {
-            max: VALI_ALIPAY_ACCOUNT.max,
-            message: VALI_ALIPAY_ACCOUNT.message
-          }
-        ],
-        reAccount: [
-          {
-            validator: validateReAccount
-          }
-        ]
-      },
-      bankCardRules: {
-        userName: [
-          {
-            required: true,
-            message: this.$t("user.bankCard_userName_required")
-          },
-          {
-            min: VALI_CARD_HOLDER.min,
-            message: VALI_CARD_HOLDER.message
-          },
-          {
-            max: VALI_CARD_HOLDER.max,
-            message: VALI_CARD_HOLDER.message
-          }
-        ],
-        bank: [
-          {
-            required: true,
-            message: this.$t("user.bankCard_bank_required")
-          }
-        ],
-        number: [
-          {
-            validator: validateBank
-          },
-          {
-            validator: validateReBank
-          }
-        ],
-        reNumber: [
-          {
-            validator: validateReNumber
-          }
-        ]
-      }
-    };
-  },
-  methods: {
-    restoreForm() {
-      this.$refs.alipayForm.resetFields();
-      this.$refs.bankCardForm.resetFields();
-    },
-    changeContent(index) {
-      this.restoreForm();
-      this.contentIndex = +index;
-    },
-    submit(index) {
-      this.$refs[index].validate(valid => {
-        if (valid) {
-          this.alipayFormLoading = true;
-          if (index === "alipayForm") {
-            this.addReceiving({
-              name: this.alipayForm.userName,
-              account: this.alipayForm.account
-            });
-          } else if (index === "bankCardForm") {
-            this.bankCardFormLoading = true;
-            this.addReceiving({
-              name: this.bankCardForm.userName,
-              account: this.bankCardForm.number,
-              bank: this.bankCardForm.bank
-            });
-          }
+      const validateBank = (rule, value, callback) => {
+        let tvalue = Number(value).toString();
+        let verify = true;
+        let sum = 0;
+        if (tvalue.length < VALI_CARD_NUMBER.min || tvalue.length > VALI_CARD_NUMBER.max) {
+          verify = false;
         }
-      });
+        // else {
+        //     for (let i = 0; i <= tvalue.length - 2; i++) {
+        //         if (i % 2 === 0) {
+        //             sum += (Number(tvalue[i]) * 2) % 10 + parseInt((Number(tvalue[i]) * 2) / 10)
+        //         } else {
+        //             sum += Number(tvalue[i])
+        //         }
+        //     }
+        //     verify = 10 - sum % 10 === Number(tvalue[tvalue.length - 1])
+        // }
+        if (!verify) {
+          callback(new Error(this.$t("user.bankCard_number_invalid")));
+        } else {
+          callback();
+        }
+      };
+      return {
+        contentIndex: 0,
+        alipayForm: {
+          userName: "",
+          account: "",
+          reAccount: ""
+        },
+        alipayFormLoading: false,
+        bankCardForm: {
+          userName: "",
+          bank: "",
+          number: "",
+          reNumber: ""
+        },
+        bankCardFormLoading: false,
+        alipayRules: {
+          userName: [
+            {
+              required: true,
+
+              message: this.$t("user.alipay_userName_required")
+            },
+            {
+              min: VALI_ALIPAY_NAME.min,
+              message: VALI_ALIPAY_NAME.message
+            },
+            {
+              max: VALI_ALIPAY_NAME.max,
+              message: VALI_ALIPAY_NAME.message
+            }
+          ],
+          account: [
+            {
+              required: true,
+              message: this.$t("user.alipay_account_required")
+            },
+            {
+              min: VALI_ALIPAY_ACCOUNT.min,
+              message: VALI_ALIPAY_ACCOUNT.message
+            },
+            {
+              max: VALI_ALIPAY_ACCOUNT.max,
+              message: VALI_ALIPAY_ACCOUNT.message
+            }
+          ],
+          reAccount: [
+            {
+              validator: validateReAccount
+            }
+          ]
+        },
+        bankCardRules: {
+          userName: [
+            {
+              required: true,
+              message: this.$t("user.bankCard_userName_required")
+            },
+            {
+              min: VALI_CARD_HOLDER.min,
+              message: VALI_CARD_HOLDER.message
+            },
+            {
+              max: VALI_CARD_HOLDER.max,
+              message: VALI_CARD_HOLDER.message
+            }
+          ],
+          bank: [
+            {
+              required: true,
+              message: this.$t("user.bankCard_bank_required")
+            }
+          ],
+          number: [
+            {
+              validator: validateBank
+            },
+            {
+              validator: validateReBank
+            }
+          ],
+          reNumber: [
+            {
+              validator: validateReNumber
+            }
+          ]
+        }
+      };
     },
-    cancel() {
-      this.restoreForm();
-      this.$emit("cancel");
-    },
-    addReceiving(requestData) {
-      this.$store
-        .dispatch("ajax_add_receiving", requestData)
-        .then(res => {
-          this.alipayFormLoading = false;
-          this.bankCardFormLoading = false;
-          if (res.data && +res.data.error === 0) {
-            // this.$Message.success(this.$t("user.receivables_add_success"));
-            this.$emit("refresh");
-            this.$emit("cancel");
-          } else {
+    methods: {
+      restoreForm() {
+        this.$refs.alipayForm.resetFields();
+        this.$refs.bankCardForm.resetFields();
+      },
+      changeContent(index) {
+        this.restoreForm();
+        this.contentIndex = +index;
+      },
+      submit(index) {
+        this.$refs[index].validate(valid => {
+          if (valid) {
+            this.alipayFormLoading = true;
+            if (index === "alipayForm") {
+              this.addReceiving({
+                name: this.alipayForm.userName,
+                account: this.alipayForm.account
+              });
+            } else if (index === "bankCardForm") {
+              this.bankCardFormLoading = true;
+              this.addReceiving({
+                name: this.bankCardForm.userName,
+                account: this.bankCardForm.number,
+                bank: this.bankCardForm.bank
+              });
+            }
+          }
+        });
+      },
+      cancel() {
+        this.restoreForm();
+        this.$emit("cancel");
+      },
+      addReceiving(requestData) {
+        this.$store
+          .dispatch("ajax_add_receiving", requestData)
+          .then(res => {
+            this.alipayFormLoading = false;
+            this.bankCardFormLoading = false;
+            if (res.data && +res.data.error === 0) {
+              // this.$Message.success(this.$t("user.receivables_add_success"));
+              this.$emit("refresh");
+              this.$emit("cancel");
+            } else {
+              // this.$Message.error(this.$t("user.receivables_add_fail"));
+            }
+          })
+          .catch(err => {
+            this.alipayFormLoading = false;
+            this.bankCardFormLoading = false;
             // this.$Message.error(this.$t("user.receivables_add_fail"));
-          }
-        })
-        .catch(err => {
-          this.alipayFormLoading = false;
-          this.bankCardFormLoading = false;
-          // this.$Message.error(this.$t("user.receivables_add_fail"));
-        });
-    },
-    getBankList() {
-      this.$store
-        .dispatch("ajax_banks")
-        .then(res => {
-          if (res.data && +res.data.error === 0) {
-            this.$store.commit("banks_setter", res.data.list || []);
-          } else {
+          });
+      },
+      getBankList() {
+        this.$store
+          .dispatch("ajax_banks")
+          .then(res => {
+            if (res.data && +res.data.error === 0) {
+              this.$store.commit("banks_setter", res.data.list || []);
+            } else {
+              // this.$Message.error(this.$t("user.banks_request_fail"));
+            }
+          })
+          .catch(err => {
             // this.$Message.error(this.$t("user.banks_request_fail"));
-          }
-        })
-        .catch(err => {
-          // this.$Message.error(this.$t("user.banks_request_fail"));
-        });
-    }
-  },
-  watch: {
-    popUpStatus(newVal) {
-      !newVal && this.restoreForm();
-    }
-  },
-  computed: {
-    bankList() {
-      return this.$store.state.banks;
-    },
-    payment_alipay_flag() {
-      let collection = this.$store.state.collection || [];
-      for (let i = 0; i < collection.length; i++) {
-        if (!collection[i].bank) {
-          this.contentIndex = 1;
-          return false;
-        }
+          });
       }
-      return true;
+    },
+    watch: {
+      popUpStatus(newVal) {
+        !newVal && this.restoreForm();
+      }
+    },
+    computed: {
+      bankList() {
+        return this.$store.state.banks;
+      },
+      payment_alipay_flag() {
+        let collection = this.$store.state.collection || [];
+        for (let i = 0; i < collection.length; i++) {
+          if (!collection[i].bank) {
+            this.contentIndex = 1;
+            return false;
+          }
+        }
+        return true;
+      }
+    },
+    mounted() {
+      this.getBankList();
+    },
+    destroyed() {
+      // this.restoreForm();
+      // this.$store.commit("layer_index_setter", 1);
+    },
+    components: {
+      logoDiv
     }
-  },
-  mounted() {
-    this.getBankList();
-  },
-  destroyed() {
-    // this.restoreForm();
-    // this.$store.commit("layer_index_setter", 1);
-  },
-  components: {
-    logoDiv
-  }
-};
+  };
 </script>
 <style lang='scss' scoped>
-#popPage {
-  // position: fixed;
-  // left: 35%;
-  // top: 5%;
-  // width: 480px;
-  // height: 472px;
-  // margin: 0 auto;
-  // z-index: 100;
-}
+  #popPage {
+    // position: fixed;
+    // left: 35%;
+    // top: 5%;
+    // width: 480px;
+    // height: 472px;
+    // margin: 0 auto;
+    // z-index: 100;
+  }
 
-.container-payment {
-  width: 480px;
-  background: #ffffff;
-  border-bottom-left-radius: 2px;
-  border-bottom-right-radius: 2px;
-}
+  .container-payment {
+    width: 480px;
+    background: #ffffff;
+    border-bottom-left-radius: 2px;
+    border-bottom-right-radius: 2px;
+  }
 
-#popPage .content {
-  padding: 45px 94px 54px;
-}
+  #popPage .content {
+    padding: 45px 94px 54px;
+  }
 
-#popPage .content div {
-  word-wrap: break-word;
-}
+  #popPage .content div {
+    word-wrap: break-word;
+  }
 
-#popPage .titleBoth {
-  height: 50px;
-  width: 480px;
-}
+  #popPage .titleBoth {
+    height: 50px;
+    width: 480px;
+  }
 
-#popPage .titleBoth .titleFocus {
-  background: #ffffff;
-}
+  #popPage .titleBoth .titleFocus {
+    background: #ffffff;
+  }
 
-#popPage .titleBoth .titleBlur {
-  background: #f4f6f9;
-}
+  #popPage .titleBoth .titleBlur {
+    background: #f4f6f9;
+  }
 
-#popPage .titleBoth .title {
-  width: 240px;
-  height: 50px;
-  float: left;
-  font-size: 16px;
-  color: #666666;
-  text-align: center;
-}
+  #popPage .titleBoth .title {
+    width: 240px;
+    height: 50px;
+    float: left;
+    font-size: 16px;
+    color: #666666;
+    text-align: center;
+  }
 
-#popPage .titleBoth .title .img {
-  display: inline-block;
-}
+  #popPage .titleBoth .title .img {
+    display: inline-block;
+  }
 
-#popPage .titleBoth .title .text {
-  display: inline-block;
-  cursor: pointer;
-  margin-top: 14px;
-}
+  #popPage .titleBoth .title .text {
+    display: inline-block;
+    cursor: pointer;
+    margin-top: 14px;
+  }
 
-#popPage .titleBoth .title .text:hover {
-  background-image: -webkit-gradient(
-    linear,
-    left top,
-    right bottom,
-    color-stop(0, #0bbfd5),
-    color-stop(1, #6dd7b2)
-  );
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
+  #popPage .titleBoth .title .text:hover {
+    background-image: -webkit-gradient(
+            linear,
+            left top,
+            right bottom,
+            color-stop(0, #0bbfd5),
+            color-stop(1, #6dd7b2)
+    );
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
 
-#popPage .titleBoth .title .text:hover:after {
-  display: block;
-  content: "";
-  left: 3px;
-  right: 3px;
-  height: 2px;
-  background-image: linear-gradient(-134deg, #0bbfd5 0%, #6dd7b2 100%);
-}
+  #popPage .titleBoth .title .text:hover:after {
+    display: block;
+    content: "";
+    left: 3px;
+    right: 3px;
+    height: 2px;
+    background-image: linear-gradient(-134deg, #0bbfd5 0%, #6dd7b2 100%);
+  }
 
-#popPage .titleBank .title {
-  margin-bottom: 20px;
-  font-size: 24px;
-  color: #666666;
-}
+  #popPage .titleBank .title {
+    margin-bottom: 20px;
+    font-size: 24px;
+    color: #666666;
+  }
 
-#popPage .submit {
-  margin-top: 40px;
-  margin-bottom: 0;
-}
+  #popPage .submit {
+    margin-top: 40px;
+    margin-bottom: 0;
+  }
 
-#popPage .alipay .form {
-  margin-top: 35px;
-}
+  #popPage .alipay .form {
+    margin-top: 35px;
+  }
 
-#popPage .alipay .submit {
-  margin-bottom: 80px - 54px;
-}
+  #popPage .alipay .submit {
+    margin-bottom: 80px - 54px;
+  }
 
-#popPage .input {
-  width: 292px;
-}
+  #popPage .input {
+    width: 292px;
+  }
 
-#popPage .submitButton {
-  width: 172px;
-}
+  #popPage .submitButton {
+    width: 172px;
+  }
 
-#popPage .cancelButton {
-  width: 110px;
-}
+  #popPage .cancelButton {
+    width: 110px;
+  }
 </style>
